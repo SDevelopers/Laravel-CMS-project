de841e91d647ab3015d555310ca8e3aecef75466
Tsvetan <tsvetan.y.petrov@gmail.com>
2017-02-05 12:55:41 +0200

    Users page working without photos

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -2,21 +2,46 @@
 
 @section('content')
 
-<h1>Create Users</h1>
+<h1>Create Users</h1> 
 
     {!! Form::open(['id'=>'create-form', 'method'=>'POST', 'action'=>'AdminUsersController@store', 'files'=>true]) !!}
     
         <div class='form-group'>
-           <p>{!! Form::label('title', 'Title') !!}</p>
-            {!! Form::text('title', null, ['class'=>'form-control']) !!}
+           <p>{!! Form::label('name', 'Name: ') !!}</p>
+            {!! Form::text('name', null, ['class'=>'form-control']) !!}
         </div>
+
+		<div class='form-group'>
+		   <p>{!! Form::label('email', 'Email: ') !!}</p>
+		    {!! Form::email('email', null, ['class'=>'form-control']) !!}
+		</div>
+
+		<div class='form-group'>
+		   <p>{!! Form::label('role_id', 'Role: ') !!}</p>
+		    {!! Form::select('role_id', ['' => 'Choose Options'] + $roles, 3, ['class'=>'form-control']) !!}
+		</div>
+
+		<div class='form-group'>
+		   <p>{!! Form::label('is_active', 'Status: ') !!}</p>
+		    {!! Form::select('is_active', [1 => 'Active', 0 => 'Not Active'], 0, ['class'=>'form-control']) !!}
+		</div>
+
+		<div class='form-group'>
+		   <p>{!! Form::label('password', 'Password: ') !!}</p>
+		    {!! Form::password('password', ['class'=>'form-control']) !!}
+		</div>
+
+		<div class='form-group'>
+		   <p>{!! Form::label('file', 'File: ') !!}</p>
+		    {!! Form::file('file', null, ['class'=>'form-control']) !!}
+		</div>
        
         <div class = 'form-group'>
-            {!!Form::submit('Create Book', ['class' => 'btn btn-primary'])!!}
+            {!!Form::submit('Create User', ['class' => 'btn btn-primary'])!!}
         </div>    
     
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-    
    	{!! Form::close() !!}
+
+   	@include('includes.form_error')
 
 @stop
