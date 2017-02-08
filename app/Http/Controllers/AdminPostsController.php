61046c588e79b3862eb6e87b5f5903f8d276e995
Tsvetan <tsvetan.y.petrov@gmail.com>
2017-02-08 23:17:20 +0200

    Small changes

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Post;
+
 class AdminPostsController extends Controller
 {
     /**
@@ -15,7 +17,9 @@ class AdminPostsController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
@@ -25,7 +29,7 @@ class AdminPostsController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create');
     }
 
     /**
